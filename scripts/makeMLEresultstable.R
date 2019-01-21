7f87ec2e99f1cd3ad114032d9c0306e4b8562bc5
Alex Stivala <alex.d.stivala@gmail.com>
2019-01-21 04:06:03 +0100

    only do simulation with zero effect for false positive estimation

--- scripts/makeMLEresultstable.R
+++ scripts/makeMLEresultstable.R
@@ -27,11 +27,11 @@ zSigma <- 2 # nominal 95% CI
 options(digits=4) # for printing rmse etc. values
 
 
-results_filenames <- c('estimnetdirected_estimates_n2000_sim.txt',
-                       'estimnetdirected_estimates_n2000_binattr.txt',
-                       'estimnetdirected_estimates_n2000_cat3.txt',
-                       'estimnetdirected_estimates_n5000_sim.txt')
-
+results_filenames <- c(#'estimnetdirected_estimates_n2000_sim.txt',
+                       'estimnetdirected_estimates_n2000_binattr.txt' 
+                       #'estimnetdirected_estimates_n2000_cat3.txt',
+                       #'estimnetdirected_estimates_n5000_sim.txt'
+                      )
 
 args <- commandArgs(trailingOnly=TRUE)
 use_sd_theta <- FALSE
