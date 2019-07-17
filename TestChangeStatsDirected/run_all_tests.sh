fd290d047b1a247e152d6d64436e5d845b009249
Alex Stivala <alex.d.stivala@gmail.com>
2019-07-17 13:09:42 +1000

    script to run all tests. all pass.

---
+++ TestChangeStatsDirected/run_all_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# File:    run_all_tests.sh
+# Author:  Alex Stivala
+# Created: July 2019
+#
+# Run all the regression  / unit tests.
+#
+
+./run_test_polblogs.sh
+./run_test_sets.sh
