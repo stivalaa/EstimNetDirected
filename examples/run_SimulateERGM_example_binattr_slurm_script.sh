44de42140a135e1c66a53459782fc2263eb4d519
Alex Stivala <alex.d.stivala@gmail.com>
2019-10-30 02:10:49 +0100

    script to run binattr simulation example

---
+++ examples/run_SimulateERGM_example_binattr_slurm_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name="SimualateERGM"
+#SBATCH --ntasks=1
+#SBATCH --time=0-4:00:00
+
+echo -n "started at: "; date
+
+ROOT=..
+
+module load R
+
+time ${ROOT}/src/SimulateERGM  sim_config_example_binattr.txt
+
+time Rscript ${ROOT}/scripts/plotSimulationDiagnostics.R stats_sim_n1000_binattr_sample.txt
+
+echo -n "ended at: "; date
+
