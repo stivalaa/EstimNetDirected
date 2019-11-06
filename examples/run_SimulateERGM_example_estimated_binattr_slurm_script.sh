a9ebed859df09ff598ad94b692f584bb93749562
Alex Stivala <alex.d.stivala@gmail.com>
2019-11-06 04:12:52 +0100

    add Gof plot

--- examples/run_SimulateERGM_example_estimated_binattr_slurm_script.sh
+++ examples/run_SimulateERGM_example_estimated_binattr_slurm_script.sh
@@ -14,5 +14,7 @@ time ${ROOT}/src/SimulateERGM  sim_config_example_estimated_binattr.txt
 
 time Rscript ${ROOT}/scripts/plotSimulationDiagnostics.R stats_estimated_sim_n1000_binattr_sample.txt  obs_stats_n1000_sample_0.txt
 
+time Rscript ${ROOT}/scripts/plotEstimNetDirectedSimFit.R  ../pythonDemo/sample_statistics_n1000_directed_binattr_sim620000000.txt simulation_estimated_sim_n1000_binattr_sample
+
 echo -n "ended at: "; date
 
