6da3da0e88e79a8405046ff8b412b924e7abbd3c
Alex Stivala <alex.d.stivala@gmail.com>
2019-11-06 01:14:40 +0100

    exampel with plotting GoF from simulation

--- examples/run_EstimNetDirected_example_contattr_slurm_script.sh
+++ examples/run_EstimNetDirected_example_contattr_slurm_script.sh
@@ -16,5 +16,7 @@ time mpirun ${ROOT}/src/EstimNetDirected_mpi config_example_contattr.txt
 time Rscript ${ROOT}/scripts/computeEstimNetDirectedCovariance.R theta_sim_n2000_cont_sample dzA_sim_n2000_cont_sample
 time Rscript ${ROOT}/scripts/plotEstimNetDirectedResults.R theta_sim_n2000_cont_sample dzA_sim_n2000_cont_sample
 
+time Rscript ${ROOT}/scripts/plotEstimNetDirectedSimFit.R sample_statistics_n2000_directed_cont_sim7920000000.txt sim_sim_n2000_cont_sample
+
 echo -n "ended at: "; date
 
---
+++ examples/run_SimulateERGM_example_estimated_contattr_slurm_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name="SimualateERGM_GoF"
+#SBATCH --ntasks=1
+#SBATCH --time=0-4:00:00
+
+echo -n "started at: "; date
+
+ROOT=..
+
+module load R
+
+time ${ROOT}/src/SimulateERGM  sim_config_example_estimated_contattr.txt
+
+time Rscript ${ROOT}/scripts/plotSimulationDiagnostics.R stats_estimated_sim_n2000_cont_sample.txt  obs_stats_n2000_cont_sample_0.txt
+
+time Rscript ${ROOT}/scripts/plotEstimNetDirectedSimFit.R  sample_statistics_n2000_directed_cont_sim7920000000.txt simulation_estimated_sim_n2000_cont_sample
+
+echo -n "ended at: "; date
+
