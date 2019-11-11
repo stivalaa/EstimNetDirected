51ae06c55b4f405b1d868e41eced74cc0e79f082
Alex Stivala <alex.d.stivala@gmail.com>
2019-11-11 17:58:06 +1100

    plot convergence tests on examples including TNT

--- examples/run_EstimNetDirected_example_slurm_script.sh
+++ examples/run_EstimNetDirected_example_slurm_script.sh
@@ -15,6 +15,7 @@ time mpirun ${ROOT}/src/EstimNetDirected_mpi config_example.txt
 
 time Rscript ${ROOT}/scripts/computeEstimNetDirectedCovariance.R theta_sim_n1000_sample dzA_sim_n1000_sample
 time Rscript ${ROOT}/scripts/plotEstimNetDirectedResults.R theta_sim_n1000_sample dzA_sim_n1000_sample
+time Rscript ${ROOT}/scripts/plotEstimNetDirectedSimFit.R ../pythonDemo/sample_statistics_n1000_directed_binattr_sim620000000.txt sim_sim_n1000_sample
 
 echo -n "ended at: "; date
 
