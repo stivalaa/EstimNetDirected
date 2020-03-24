167c64b23d98c873e9f8024855b7cc9e3142c452
Alex Stivala <alex.d.stivala@gmail.com>
2020-03-24 12:14:45 +1100

    include converged and total runs in LaTeX table

--- scripts/estimnetdirectedEstimation2textableMultiModels.sh
+++ scripts/estimnetdirectedEstimation2textableMultiModels.sh
@@ -67,53 +67,58 @@ model=1
 for estimationresults in $*
 do
     # https://unix.stackexchange.com/questions/78472/print-lines-between-start-and-end-using-sed
-    cat ${estimationresults} | sed -n -e '/^Pooled/,/^TotalRuns/{//!p}'  | tr -d '*' | fgrep -vw AcceptanceRate | fgrep -vw TotalRuns | fgrep -vw ConvergedRuns | awk '{print $1,$2,$4,$5}'  |  tr ' ' '\t' | sed "s/^/${model}\t/" >> ${estimnet_tmpfile}
+    cat ${estimationresults} | sed -n -e '/^Pooled/,${//!p}'  | tr -d '*' | fgrep -vw AcceptanceRate | awk '{print $1,$2,$4,$5}'  |  tr ' ' '\t' | sed "s/^/${model}\t/" >> ${estimnet_tmpfile}
     model=`expr $model + 1`
 done
 
 
-effectlist=`cat ${estimnet_tmpfile} |  awk '{print $2}' | sort | uniq`
+effectlist=`cat ${estimnet_tmpfile} | fgrep -vw TotalRuns | fgrep -vw ConvergedRuns | awk '{print $2}' | sort | uniq`
 
-for effect in ${effectlist}
+for effect in ${effectlist} ConvergedRuns TotalRuns
 do
-  model=1
-  echo -n "${effect} " | tr '_' ' '
-  while [ $model -le $num_models ]; 
-  do
-    estimnet_point=`grep -w ${effect} ${estimnet_tmpfile} | awk -vmodel=$model '$1 == model {print $3}'`
-    estimnet_stderr=`grep -w ${effect} ${estimnet_tmpfile} | awk -vmodel=$model '$1 == model {print $4}'`
-    estimnet_tratio=`grep -w ${effect} ${estimnet_tmpfile} | awk -vmodel=$model '$1 == model {print $5}'`
-    if [ "${estimnet_point}" == "" ];  then
-      echo -n " & ---"
-    else 
-      # put statistically significant results in normal text, others
-      # in "\light" which must be defined as e.g.
-      # \newcommand{\light}[1]{\textcolor{gray}{#1}}
-      # which requires \usepackage{xcolor} (NB color package does not have gray)
-      
-      # bc cannot handle scientific notation so use sed to convert it 
-      estimnet_lower=`echo "${estimnet_point} - ${zSigma} * ${estimnet_stderr}" | sed -e 's/[eE]+*/*10^/' | bc -l`
-      estimnet_upper=`echo "${estimnet_point} + ${zSigma} * ${estimnet_stderr}" | sed -e 's/[eE]+*/*10^/' | bc -l`
-      estimnet_point=`echo "${estimnet_point}" | sed -e 's/[eE]+*/*10^/'`
-      estimnet_tratio=`echo "${estimnet_tratio}" | sed -e 's/[eE]+*/*10^/'`
-      estimnet_stderr=`echo "${estimnet_stderr}" | sed -e 's/[eE]+*/*10^/'`
-      echo AAA "${estimnet_point}">&2
-      abs_estimate=`echo "if (${estimnet_point} < 0) -(${estimnet_point}) else ${estimnet_point}" | bc -l`
-      abs_tratio=`echo "if (${estimnet_tratio} < 0) -(${estimnet_tratio}) else ${estimnet_tratio}" | bc -l`
-      echo YYY ${abs_estimate} >&2
-      echo QQQ ${abs_tratio} >&2
-      echo XXX "${abs_tratio} <= ${tratioThreshold} && ${abs_estimate} > ${zSigma} * ${estimnet_stderr}" >&2
-      signif=`echo "${abs_tratio} <= ${tratioThreshold} && ${abs_estimate} > ${zSigma} * ${estimnet_stderr}" | bc -l`
-      echo ZZZ ${signif} >&2 
-      if [ ${signif} -eq 0 ]; then
-        printf ' & $\\light{\\underset{(%.3f, %.3f)}{%.3f}}$' ${estimnet_lower} ${estimnet_upper} ${estimnet_point}
-      else
-        printf ' & $\\underset{(%.3f, %.3f)}{%.3f}$' ${estimnet_lower} ${estimnet_upper} ${estimnet_point}
-      fi
-    fi
-    model=`expr $model + 1`
-  done
-  echo '\\'
+    model=1
+    echo -n "${effect} " | tr '_' ' '
+    while [ $model -le $num_models ]; 
+    do
+        if [ ${effect} = "ConvergedRuns" -o ${effect} = "TotalRuns" ]; then
+            runs=`grep -w ${effect} ${estimnet_tmpfile} | awk -vmodel=$model '$1 == model {print $3}'`
+            echo -n " & ${runs}"
+        else
+            estimnet_point=`grep -w ${effect} ${estimnet_tmpfile} | awk -vmodel=$model '$1 == model {print $3}'`
+            estimnet_stderr=`grep -w ${effect} ${estimnet_tmpfile} | awk -vmodel=$model '$1 == model {print $4}'`
+            estimnet_tratio=`grep -w ${effect} ${estimnet_tmpfile} | awk -vmodel=$model '$1 == model {print $5}'`
+            if [ "${estimnet_point}" == "" ];  then
+                echo -n " & ---"
+            else 
+                # put statistically significant results in normal text, others
+                # in "\light" which must be defined as e.g.
+                # \newcommand{\light}[1]{\textcolor{gray}{#1}}
+                # which requires \usepackage{xcolor} (NB color package does not have gray)
+                
+                # bc cannot handle scientific notation so use sed to convert it 
+                estimnet_lower=`echo "${estimnet_point} - ${zSigma} * ${estimnet_stderr}" | sed -e 's/[eE]+*/*10^/' | bc -l`
+                estimnet_upper=`echo "${estimnet_point} + ${zSigma} * ${estimnet_stderr}" | sed -e 's/[eE]+*/*10^/' | bc -l`
+                estimnet_point=`echo "${estimnet_point}" | sed -e 's/[eE]+*/*10^/'`
+                estimnet_tratio=`echo "${estimnet_tratio}" | sed -e 's/[eE]+*/*10^/'`
+                estimnet_stderr=`echo "${estimnet_stderr}" | sed -e 's/[eE]+*/*10^/'`
+                echo AAA "${estimnet_point}">&2
+                abs_estimate=`echo "if (${estimnet_point} < 0) -(${estimnet_point}) else ${estimnet_point}" | bc -l`
+                abs_tratio=`echo "if (${estimnet_tratio} < 0) -(${estimnet_tratio}) else ${estimnet_tratio}" | bc -l`
+                echo YYY ${abs_estimate} >&2
+                echo QQQ ${abs_tratio} >&2
+                echo XXX "${abs_tratio} <= ${tratioThreshold} && ${abs_estimate} > ${zSigma} * ${estimnet_stderr}" >&2
+                signif=`echo "${abs_tratio} <= ${tratioThreshold} && ${abs_estimate} > ${zSigma} * ${estimnet_stderr}" | bc -l`
+                echo ZZZ ${signif} >&2 
+                if [ ${signif} -eq 0 ]; then
+                    printf ' & $\\light{\\underset{(%.3f, %.3f)}{%.3f}}$' ${estimnet_lower} ${estimnet_upper} ${estimnet_point}
+                else
+                    printf ' & $\\underset{(%.3f, %.3f)}{%.3f}$' ${estimnet_lower} ${estimnet_upper} ${estimnet_point}
+                fi
+            fi
+        fi
+        model=`expr $model + 1`
+    done
+    echo '\\'
 done
 
 echo '\hline'  
