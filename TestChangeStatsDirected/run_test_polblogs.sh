8a1e0c5d0923d956a3f8c7345420173f83940297
Alex Stivala <alex.d.stivala@gmail.com>
2020-04-20 15:38:16 +1000

    add tests for no two-path lookup

--- TestChangeStatsDirected/run_test_polblogs.sh
+++ TestChangeStatsDirected/run_test_polblogs.sh
@@ -6,10 +6,11 @@
 rc=0
 
 BASELINE=polblogs_test_results_baseline.txt
+
 OUTPUT=polblogs_test_results.out
 DIFFILE=polblogs_test_results.diff
 
-echo "1. two-path matrices"
+echo "1. no two-path lookup"
 
 time ./testChangeStatsDirected ../pythonDemo/polblogs/polblogs_arclist.txt  polblogs_nodepairs.txt | fgrep -v nnz | fgrep -v DEBUG  > ${OUTPUT}
 
@@ -25,7 +26,26 @@ else
   rc=1
 fi
 
-echo "2. two-path hash tables"
+OUTPUT=polblogs_test_results_array.out
+DIFFILE=polblogs_test_results_array.diff
+
+echo "2. two-path matrices"
+
+time ./testChangeStatsDirected_array ../pythonDemo/polblogs/polblogs_arclist.txt  polblogs_nodepairs.txt | fgrep -v nnz | fgrep -v DEBUG  > ${OUTPUT}
+
+diff ${BASELINE} ${OUTPUT} > ${DIFFILE}
+
+if [ $? -eq 0 ]; then
+  echo
+  echo "PASSED"
+else 
+  echo
+  echo "**** FAILED ****"
+  echo "diff results are in ${DIFFILE}"
+  rc=1
+fi
+
+echo "3. two-path hash tables"
 
 OUTPUT=polblogs_test_results_hash.out
 DIFFILE=polblogs_test_results_hash.diff
