ad175207d52d9054c459501b0c0191e328ecaead
Alex Stivala <alex.d.stivala@gmail.com>
2020-04-23 04:26:45 +0200

    new scripts for comparing two-path tables/notables

---
+++ examples/run_EstimNetDirected_notables_polblogs_slurm_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name="EstimNetDirected_mpi_hash"
+#SBATCH --ntasks=8
+#SBATCH --time=0-1:00:00
+
+echo -n "started at: "; date
+
+ROOT=..
+
+module load openmpi
+module load R/3.2.5
+
+time mpirun ${ROOT}/src/EstimNetDirected_mpi config_polblogs.txt
+
+time Rscript ${ROOT}/scripts/computeEstimNetDirectedCovariance.R theta_polblogs dzA_polblogs
+time Rscript ${ROOT}/scripts/plotEstimNetDirectedResults.R theta_polblogs dzA_polblogs
+
+echo -n "ended at: "; date
+
