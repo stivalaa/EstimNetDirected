2985c4354956c097b3cf9af7fe2e8898b09a5032
Alex Stivala <alex.d.stivala@gmail.com>
2020-10-21 09:19:24 +0200

    set lugsail parametr r=1 to get vanilla batch means estimator as before

--- scripts/computeEstimNetDirectedCovariance.R
+++ scripts/computeEstimNetDirectedCovariance.R
@@ -227,7 +227,12 @@ if (keptcount > 0 ) {
       ## other methods.)
       ## Instead use the (default) "batch means"
       ## method (Jones 2006; Vats et al., 2017; Vats et al., 2018)
-      mcerror <- mcse.multi(x = this_theta, r=2, method="bm")
+      ## Note the r ("lugsail") parameter was only introduced in
+      ## mcmcse R package version 1.4-1 (January 2020); we set it to r=1
+      ## to get the "vanilla" estimator so it should behave as it did
+      ## before the new version.
+      ## See https://cran.r-project.org/web/packages/mcmcse/vignettes/mcmcse_vignette.pdf (January 29, 2020)
+      mcerror <- mcse.multi(x = this_theta, r=1, method="bm")
       est_theta <- mcerror$est    # point estimate (mean)
       Nmcmc <- nrow(this_theta) # number of MCMC samples
       ## mcse.multi returns asymptotic covariance matrix so need to divide
@@ -237,7 +242,7 @@ if (keptcount > 0 ) {
       mcmc_cov <- mcerror$cov / Nmcmc  # covariance matrix
   
       ## covariance matrix for ERGM MLE error
-      mcerror_dz <- mcse.multi(x = this_dzA, r=2, method="bm")
+      mcerror_dz <- mcse.multi(x = this_dzA, r=1, method="bm")
       stopifnot(mcerror_dz$nsim == Nmcmc)
       acov <- mcerror_dz$cov / Nmcmc
       mle_cov = solve(acov) # solve(A) is matrix inverse of A
