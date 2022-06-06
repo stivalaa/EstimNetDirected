b67cac536112b7faf2a54040b4182afcd17fa3c6
Alex Stivala <alex.d.stivala@gmail.com>
2022-06-06 12:25:13 +1000

    script to run simulation example

---
+++ examples/bipartite/simulation/run_SimulateERGM_bipartite_Example_A12000_B4000_attrs_tnt_slurm_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name="SimulateERGM"
+#SBATCH --ntasks=1
+#SBATCH --time=0-0:30:00
+
+echo -n "started at: "; date
+uname -a
+
+ROOT=../../..
+
+module load r
+
+time ${ROOT}/src/SimulateERGM  config_sim_bipartite_example_A12000_B4000_attrs_tnt.txt
+
+time Rscript ${ROOT}/scripts/plotSimulationDiagnostics.R stats_sim_bipartite_A12000_B4000_attrs_sampler_tnt.txt
+
+echo -n "ended at: "; date
+
