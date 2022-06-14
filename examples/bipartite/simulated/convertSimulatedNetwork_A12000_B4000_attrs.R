e1dbaf39695cc6e502a7abd6c0d0fd026bc97306
Alex Stivala <alex.d.stivala@gmail.com>
2022-06-14 04:59:10 +0200

    example attr bipartite from BPNet, verified obs stats sama as BPNet, OK

---
+++ examples/bipartite/simulated/convertSimulatedNetwork_A12000_B4000_attrs.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+#
+# Read bipartite network simulated by BPNet and write in simple bipartite
+# Pajek .net format for EStimNetDirected
+#
+source('../../../scripts/snowballSample.R')
+
+g <- read_graph_file('sample-statistics-A12000_B4000_attrs_sim830000000.txt')
+g <- remove.edge.attribute(g, 'weight')
+write.graph(g, 'bpnet_A12000_B4000_attrs_sim830000000.net', format='pajek')
