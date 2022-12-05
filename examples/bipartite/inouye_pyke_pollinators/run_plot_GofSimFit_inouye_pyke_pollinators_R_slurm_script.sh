1446078545c0ffb1a344e7f3cd64cbaa076c13b2
Alex Stivala <alex.d.stivala@gmail.com>
2022-12-06 00:53:54 +0100

    use multicore on example GoF plot script

--- examples/bipartite/inouye_pyke_pollinators/run_plot_GofSimFit_inouye_pyke_pollinators_R_slurm_script.sh
+++ examples/bipartite/inouye_pyke_pollinators/run_plot_GofSimFit_inouye_pyke_pollinators_R_slurm_script.sh
@@ -2,8 +2,9 @@
 
 #SBATCH --job-name="plotGofSimFit-inouye_pyke_pollinators"
 #SBATCH --ntasks=1
-#SBATCH --time=0-00:10:00
-#SBATCH --mem-per-cpu=8GB
+#SBATCH --time=0-01:00:00
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=2GB
 #SBATCH --partition=slim
 #SBATCH --output=PlotGofSimFit-inouye_pyke_pollinators-%j.out
 #SBATCH --error=PlotGofSimFit-inouye_pyke_pollinators-%j.err
@@ -13,6 +14,10 @@ echo -n "started at: "; date
 ROOT=${HOME}/EstimNetDirected
 
 module load r
+
+# for R library(parallel) mclapply()
+export MC_CORES=${SLURM_CPUS_ON_NODE}
+echo MC_CORES = $MC_CORES
 
 time Rscript ${ROOT}/scripts/plotEstimNetDirectedSimFit.R -y -t  inouye_pyke_pollinators_bipartite.net sim_gof_inouye_pyke_pollinators
 
