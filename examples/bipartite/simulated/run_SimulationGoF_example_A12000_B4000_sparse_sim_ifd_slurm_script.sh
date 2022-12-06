acfa60f5e3a90e32c4e23c2ea9d519a6ef3324cc
Alex Stivala <alex.d.stivala@gmail.com>
2022-12-06 01:50:53 +0100

    do cycle distribution (use parallel cores)

--- examples/bipartite/simulated/run_SimulationGoF_example_A12000_B4000_sparse_sim_ifd_slurm_script.sh
+++ examples/bipartite/simulated/run_SimulationGoF_example_A12000_B4000_sparse_sim_ifd_slurm_script.sh
@@ -2,8 +2,10 @@
 
 #SBATCH --job-name="SimulationGoF_mpi_ifd_bpnet_A12000_B4000_sim"
 #SBATCH --ntasks=1
-#SBATCH --time=0-0:20:00
-#SBATCH --mem-per-cpu=4GB
+#SBATCH --cpus-per-task=20
+#SBATCH --time=0-1:00:00
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --partition=slim
 #SBATCH --output=SimulationGoF_ifd_bpnet_A12000_B4000_sim-%j.out
 #SBATCH --error=SimulationGoF_ifd_bpnet_A12000_B4000_sim-%j.err
 
@@ -27,9 +29,13 @@ time ${ROOT}/src/SimulateERGM ${gof_sim_config}
 
 module load r 
 
+# for R library(parallel) mclapply()
+export MC_CORES=${SLURM_CPUS_ON_NODE}
+echo MC_CORES = $MC_CORES
+
 time Rscript ${ROOT}/scripts/plotSimulationDiagnostics.R stats_ifd_bpnet_A12000_B4000_sim.txt obs_stats_ifd_bpnet_A12000_B4000_sim_0.txt 
 
-time Rscript ${ROOT}/scripts/plotEstimNetDirectedSimFit.R bpnet_A12000_B4000_sparse_sim770000000.net sim_gof_ifd_bpnet_A12000_B4000_sim
+time Rscript ${ROOT}/scripts/plotEstimNetDirectedSimFit.R -y -s bpnet_A12000_B4000_sparse_sim770000000.net sim_gof_ifd_bpnet_A12000_B4000_sim
 
 
 echo -n "ended at: "; date
