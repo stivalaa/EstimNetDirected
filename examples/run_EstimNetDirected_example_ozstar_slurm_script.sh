0f60dbd3b4cfe3bf7706c8e64ac21957f2d25981
Alex Stivala <alex.d.stivala@gmail.com>
2023-06-13 14:05:03 +1000

    update for new modules on new Ngarrgu Tindebeek system

--- examples/run_EstimNetDirected_example_ozstar_slurm_script.sh
+++ examples/run_EstimNetDirected_example_ozstar_slurm_script.sh
@@ -9,12 +9,15 @@ echo -n "started at: "; date
 
 ROOT=..
 
-module load gcc/9.2.0     # need this for 'toolchain' hierarchical modules
-module load openmpi/4.0.2 # module version numbers are required on OzStar
+module load foss/2022b
+module load python/3.10.8
+module load numpy/1.24.2-scipy-bundle-2023.02
 
 time mpirun ${ROOT}/src/EstimNetDirected_mpi config_example.txt
 
-module load r/4.1.1 # module version numbers are required on OzStar
+module load gcc/11.3.0    # needed by r/4.2.1
+module load openmpi/4.1.4 # needed by r/4.2.1
+module load r/4.2.1
 
 time Rscript ${ROOT}/scripts/computeEstimNetDirectedCovariance.R theta_sim_n1000_sample dzA_sim_n1000_sample
 time Rscript ${ROOT}/scripts/plotEstimNetDirectedResults.R theta_sim_n1000_sample dzA_sim_n1000_sample
