2513563d0bb399da8caf66fe74b65f0cda5aa990
Alex Stivala <alex.d.stivala@gmail.com>
2023-12-21 12:47:28 +1100

    use only 10 not 100 grid pionts

--- Test/TestChangeStatsAttrBipartite/run_test_b1nodematch_bpnet_A12000_B4000_attr.sh
+++ Test/TestChangeStatsAttrBipartite/run_test_b1nodematch_bpnet_A12000_B4000_attr.sh
@@ -11,9 +11,9 @@ echo "Bipartite nodematch (alpha and beta)"
 for implementation in ./testBipartiteAlphaBetaChangeStats ./testBipartiteAlphaBetaChangeStats_array ./testBipartiteAlphaBetaChangeStats_hash
 do
   echo ${implementation}
-  time for i in `seq 0 100`
+  for i in `seq 0 10`
   do
-    exponent=`echo "$i / 100" | bc -l`
+    exponent=`echo "$i / 10" | bc -l`
     ${implementation} ../../examples/bipartite/simulated/bpnet_A12000_B4000_attrs_sim830000000.net ../../examples/bipartite/simulation/catattr_all.txt $exponent
     if [ $? -ne 0 ]; then
       echo FAILED
