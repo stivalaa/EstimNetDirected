dc39b6a3c6f28220243ca2ba161f9aa89395948a
Alex Stivala <alex.d.stivala@gmail.com>
2023-12-22 11:28:49 +1100

    run stats compare in test script

--- Test/TestChangeStatsAttrBipartite/run_test_b1nodematch_bpnet_A12000_B4000_attr.sh
+++ Test/TestChangeStatsAttrBipartite/run_test_b1nodematch_bpnet_A12000_B4000_attr.sh
@@ -6,20 +6,28 @@
 ## changeBipartiteNodematchBetaB, changeBipartiteNodematchAlphaA, and
 ## changeBipartiteNodematchAlphaB in EstimNetDirected.
 
+BASELINE=statnet_b1nodematch_baseline.out
+OUTPUT=b1nodematch_test.out
 
 echo "Bipartite nodematch (alpha and beta)"
 for implementation in ./testBipartiteAlphaBetaChangeStats ./testBipartiteAlphaBetaChangeStats_array ./testBipartiteAlphaBetaChangeStats_hash
 do
-  echo ${implementation}
-  for i in `seq 0 10`
-  do
-    exponent=`echo "$i / 10" | bc -l`
-    ${implementation} ../../examples/bipartite/simulated/bpnet_A12000_B4000_attrs_sim830000000.net ../../examples/bipartite/simulation/catattr_all.txt $exponent
+    echo ${implementation}
+    cat /dev/null > ${OUTPUT}
+    for i in `seq 0 10`
+    do
+        exponent=`echo "$i / 10" | bc -l`
+        ${implementation} ../../examples/bipartite/simulated/bpnet_A12000_B4000_attrs_sim830000000.net ../../examples/bipartite/simulation/catattr_all.txt $exponent >> ${OUTPUT}
+        if [ $? -ne 0 ]; then
+            echo "**** FAILED ****"
+            exit 1
+        fi
+    done
+    ./compareStatsValues.py ${BASELINE} ${OUTPUT}
     if [ $? -ne 0 ]; then
-      echo FAILED
-      exit 1
+        echo "**** FAILED ****"
+        exit 1
     fi
-  done
 done
 echo PASSED
 exit 0
