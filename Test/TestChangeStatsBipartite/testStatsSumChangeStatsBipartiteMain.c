46df2631a6d803371a8801974ea30d9f39677ed5
Alex Stivala <alex.d.stivala@gmail.com>
2024-01-22 16:05:55 +1100

    BipariteAltKCyclesA_SLOW appears to work now (but extremely SLOW)

--- Test/TestChangeStatsBipartite/testStatsSumChangeStatsBipartiteMain.c
+++ Test/TestChangeStatsBipartite/testStatsSumChangeStatsBipartiteMain.c
@@ -36,10 +36,73 @@
 #include "changeStatisticsBipartiteUndirected.h"
 #include "loadGraph.h"
 
+
+
+/*****************************************************************************
+ *
+ * utility functions
+ *
+ ****************************************************************************/
+
 /* Approximate double floating point equality */
 #define DOUBLE_APPROX_EQ_TEST(a, b) ( fabs((a) - (b)) <= 1e-06 )
 
+/*
+ * Binomial coefficient n choose k
+ */
+static ulonglong_t n_choose_k(uint_t n, uint_t k)
+{
+  uint_t i;
+  ulonglong_t a = 1, b = 1, l = k;
 
+  if (n < k) {
+    return 0;
+  }
+
+  if (n - k < k) {
+    l = n - k;
+  }
+  
+  for (i = 1; i <= l; i++) {
+    a *= (n + 1 - i);
+    b *= i;
+  }
+  /*printf("%u %u %llu %llu %llu\n", n, k, a, b, a/b);*/
+  assert(a % b == 0);
+  return a / b;
+}
+
+/*
+ * count k-two-paths, as defined by eqn (6.11) in
+ *
+ *   Wang, P., Sharpe, K., Robins, G. L., & Pattison,
+ *   P. E. (2009). Exponential random graph (p∗) models for affiliation
+ *   networks. Social Networks, 31(1), 12-25.
+ *
+ */
+static ulonglong_t k_two_paths_A(const graph_t *g, uint_t k)
+{
+  uint_t l,i;
+  ulonglong_t count = 0;
+
+  assert(k > 0);
+
+  for (l = g->num_A_nodes + 1; l < g->num_A_nodes + g->num_B_nodes; l++){
+    for (i = g->num_A_nodes; i < l; i++) {
+      assert(bipartite_node_mode(g, i) == MODE_B);
+      assert(bipartite_node_mode(g, l) == MODE_B);
+      count += n_choose_k(GET_B2PATH_ENTRY(g, i, l), k);
+    }
+  }  
+  if (k == 2) {
+    return count;//XXX
+    assert(count % 2 == 0);
+    return count / 2;
+  }  else {
+    return count;
+  }
+}
+ 
 
 /*****************************************************************************
  *
@@ -123,6 +186,44 @@ static double BipartiteAltKCyclesB(const graph_t *g, double lambda)
   return lambda * value;
 }
 
+
+
+
+/*
+ * Statistic for BipartiteAltKCyclesA, alternating k-cycles for type A
+ * nodes (K-Ca in BPNet, XACA in MPNet), alternative (inefficient)
+ * implementation, defined by eqn (6.12) in:
+ *
+ *   Wang, P., Sharpe, K., Robins, G. L., & Pattison,
+ *   P. E. (2009). Exponential random graph (p∗) models for affiliation
+ *   networks. Social Networks, 31(1), 12-25.
+ *
+ *
+ * Parameters:
+ *     g      - undirected bipartite graph
+ *     lambda - decay value > 1.0
+ *
+ * Return value:
+ *      statistic for g with decay value lambda
+
+ */
+static double BipartiteAltKCyclesA_SLOW(const graph_t *g, double lambda)
+{
+  uint_t i;
+  double value;
+
+  assert (lambda > 1.0);
+
+  assert(g->is_bipartite);
+  assert(!g->is_directed);
+
+  value = k_two_paths_A(g, 1) - 1*k_two_paths_A(g, 2)/lambda;
+
+  for (i = 3; i < g->num_A_nodes + g->num_B_nodes - 2; i++) {
+    value += pow(-1/lambda, i-1) * k_two_paths_A(g, i);
+  }
+  return value;
+}
 
 /*****************************************************************************
  *
@@ -208,6 +309,11 @@ int main(int argc, char *argv[])
   /*fprintf(stderr,"stat_value   = %.10f\nobs_stats[0] = %.10f\n", stat_value, obs_stats[0]);*/
   /*fprintf(stderr, "diff = %g\n", fabs((stat_value) - (obs_stats[0])));*/
   assert(DOUBLE_APPROX_EQ_TEST(stat_value,  obs_stats[0]));
+  stat_value = BipartiteAltKCyclesA_SLOW(g, lambda_values[0]);
+  fprintf(stderr,"stat_value   = %.10f\nobs_stats[0] = %.10f\n", stat_value, obs_stats[0]);
+  fprintf(stderr, "diff = %g\n", fabs((stat_value) - (obs_stats[0])));
+  assert(DOUBLE_APPROX_EQ_TEST(stat_value,  obs_stats[0]));
+  
 
     stat_value = BipartiteAltKCyclesB(g, lambda_values[1]);
   /*fprintf(stderr,"stat_value   = %.10f\nobs_stats[1] = %.10f\n", stat_value, obs_stats[0]);*/
