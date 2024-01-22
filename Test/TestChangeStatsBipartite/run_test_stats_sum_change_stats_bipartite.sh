52d78913dc577fd73eeaa34b07ea550bb8c4e7f6
Alex Stivala <alex.d.stivala@gmail.com>
2024-01-22 12:55:25 +1100

    rearrange loops so not duplicating tests

--- Test/TestChangeStatsBipartite/run_test_stats_sum_change_stats_bipartite.sh
+++ Test/TestChangeStatsBipartite/run_test_stats_sum_change_stats_bipartite.sh
@@ -10,14 +10,14 @@ echo "Testing summing change statistics for bipartite networks"
 for implementation in ./testStatsSumChangeStatsBipartite ./testStatsSumChangeStatsBipartite_array ./testStatsSumChangeStatsBipartite_hash
 do
     echo ${implementation}
-    for i in `seq 1 50`
+    for netfile in robertson_pollinators_bipartite.net ../../examples/bipartite/inouye_pyke_pollinators/inouye_pyke_pollinators_bipartite.net ../../examples/bipartite/simulated/bpnet_A6000_B750_sparse_sim100000000.net  ../../examples/bipartite/simulated/bpnet_A12000_B4000_attrs_sim830000000.net
     do
-        for netfile in robertson_pollinators_bipartite.net ../../examples/bipartite/inouye_pyke_pollinators/inouye_pyke_pollinators_bipartite.net ../../examples/bipartite/simulated/bpnet_A6000_B750_sparse_sim100000000.net  ../../examples/bipartite/simulated/bpnet_A12000_B4000_attrs_sim830000000.net
+        echo ${netfile}
+        for i in `seq 1 50`
         do
-            echo ${netfile}
             lambda=`echo "1 + $i / 10" | bc -l`
             echo lambda = ${lambda}
-            ${implementation} ${netfile}  ${lambda}
+            ${implementation} ${netfile}  ${lambda} > /dev/null
             if [ $? -ne 0 ]; then
                 echo "**** FAILED ****"
                 exit 1
