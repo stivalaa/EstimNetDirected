0b5f92b9471ab850d74d67119ce324168b91b7b0
Alex Stivala <alex.d.stivala@gmail.com>
2024-01-22 12:57:55 +1100

    add new tests to run all tests script

--- Test/TestChangeStatsBipartite/run_all_bipartite_tests.sh
+++ Test/TestChangeStatsBipartite/run_all_bipartite_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# File:    run_all_tests.sh
+# File:    run_all_bipartite_tests.sh
 # Author:  Alex Stivala
 # Created: May 2022 
 #
@@ -10,3 +10,4 @@
 ./run_test_bpnet_A6000_B750.sh
 ./run_test_robertson_pollinators.sh
 ./run_test_inouye_pyke_pollinators.sh
+./run_test_stats_sum_change_stats_bipartite.sh
