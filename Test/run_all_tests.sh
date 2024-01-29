e3aa6a02f9a2afb7e8e959ed89542c84a8d36d19
Alex Stivala <alex.d.stivala@gmail.com>
2024-01-29 17:10:09 +1100

    fix toplevel test script to use new script in undirected

--- Test/run_all_tests.sh
+++ Test/run_all_tests.sh
@@ -8,7 +8,7 @@
 #
 fail=0
 (cd TestChangeStatsDirected && ./run_test_polblogs.sh) || fail=1
-(cd TestChangeStatsUndirected && ./run_all_tests.sh) || fail=1
+(cd TestChangeStatsUndirected && ./run_all_undirected_tests.sh) || fail=1
 (cd TestSetFunctions && ./run_test_sets.sh) || fail=1
 (cd TestChangeStatsBipartite && ./run_all_bipartite_tests.sh) || fail=1
 (cd TestChangeStatsAttrBipartite && ./run_all_attr_bipartite_tests.sh) || fail=1
