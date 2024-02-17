d052d683c082940b3bd5d8e783afb026b4af6d3f
Alex Stivala <alex.d.stivala@gmail.com>
2024-02-17 16:07:38 +1100

    slurm script to run regerssino tests on ozstar (nt)

---
+++ Test/run_all_tests_slurm_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name="EstimNetDirected_regression_tests"
+#SBATCH --ntasks=1
+#SBATCH --time=0-02:00:00
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --output=EstimNetDirected_regression_tests-%j.out
+#SBATCH --error=EstimNetDirected_regression_tests-%j.err
+
+echo -n "started at: "; date
+
+module purge
+module load gcc/11.3.0     # need this for 'toolchain' hierarchical modules
+module load openmpi/4.1.4 # module version numbers are required on OzStar
+module load r/4.2.1
+
+time ./run_all_tests.sh
+
+echo -n "ended at: "; date
+
