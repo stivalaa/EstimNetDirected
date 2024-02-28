4dd23ab7d670a2677db50e040cbc79bd6312b5d0
Alex Stivala <alex.d.stivala@gmail.com>
2024-02-28 13:23:04 +1100

    increase memory limit

--- examples/bipartite/inouye_pyke_pollinators/run_EstimNetDirected_inouye_pyke_pollinators_slurm_script.sh
+++ examples/bipartite/inouye_pyke_pollinators/run_EstimNetDirected_inouye_pyke_pollinators_slurm_script.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="EstimNetDirected_mpi-ifd_inouye_pyke_pollinators"
 #SBATCH --ntasks=20
 #SBATCH --time=0-04:00:00
-#SBATCH --mem-per-cpu=8MB
+#SBATCH --mem-per-cpu=600MB
 #SBATCH --output=EstimNetDirected-ifd_inouye_pyke_pollinators-%j.out
 #SBATCH --error=EstimNetDirected-ifd_inouye_pyke_pollinators-%j.err
 
