d7b21e85d680c4cca12b0311f36515019fcff967
Alex Stivala <alex.d.stivala@gmail.com>
2024-09-13 09:31:48 +1000

    lower cycle limit and no Opsahl c.c. as too slow

--- examples/bipartite/simulation/run_SimulateERGM_BipartiteFourCyclesNodePower_examples.sh
+++ examples/bipartite/simulation/run_SimulateERGM_BipartiteFourCyclesNodePower_examples.sh
@@ -24,7 +24,8 @@ Rscript plot_BipartiteFourCyclesNodePower_simulation_graph_visualizations.R
 for prefix in simulation_sim_bipartite_FourCyclesNodePower_Anegative_Bnegative simulation_sim_bipartite_FourCyclesNodePower_Anegative_Bpositive simulation_sim_bipartite_FourCyclesNodePower_Apositive_Bnegative simulation_sim_bipartite_FourCyclesNodePower_Apositive_Bpositive simulation_sim_bipartite_FourCyclesNodePower_Apositive_Bzero simulation_sim_bipartite_FourCyclesNodePower_Azero_Bpositive simulation_sim_bipartite_FourCyclesNodePower_Azero_Bzero
 do
   echo ========== ${prefix} ==========
-  Rscript ${ROOT}/scripts/plotEstimNetDirectedSimFit.R -t -y 16 ${prefix}_9900000.net ${prefix}
+  ## -t and -y 16 too slow on higher density / higher four-cycles graphs
+  Rscript ${ROOT}/scripts/plotEstimNetDirectedSimFit.R  -y 8 ${prefix}_9900000.net ${prefix}
 done
 
 echo -n "ended at: "; date
