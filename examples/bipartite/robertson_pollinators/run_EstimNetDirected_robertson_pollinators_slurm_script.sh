e1966c1a3a2f94ad017ce17116385bab890c8fc8
Alex Stivala <alex.d.stivala@gmail.com>
2024-11-26 16:10:24 +1100

    Model with BipartiteAltK4CyclesB simulation shows not converged uses only a few minutes estimation time, far faster than BipartiteNodePowerFourCycles

--- examples/bipartite/robertson_pollinators/run_EstimNetDirected_robertson_pollinators_slurm_script.sh
+++ examples/bipartite/robertson_pollinators/run_EstimNetDirected_robertson_pollinators_slurm_script.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name="EstimNetDirected_mpi-ifd_robertson_pollinators"
 #SBATCH --ntasks=20
-#SBATCH --time=0-60:00:00
+#SBATCH --time=0-01:00:00
 #SBATCH --mem-per-cpu=500MB
 #SBATCH --output=EstimNetDirected-ifd_robertson_pollinators-%j.out
 #SBATCH --error=EstimNetDirected-ifd_robertson_pollinators-%j.err
