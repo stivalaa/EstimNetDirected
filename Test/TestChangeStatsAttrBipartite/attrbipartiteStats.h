7bbb220d770f7cdeb99d265699771d2acbdb4cce
Alex Stivala <alex.d.stivala@gmail.com>
2025-01-21 10:05:09 +1100

    move statistic functions into new module attrbipartiteStats

---
+++ Test/TestChangeStatsAttrBipartite/attrbipartiteStats.h
@@ -0,0 +1,26 @@
+#ifndef ATTRBIPARTITESTATS_H
+#define ATTRBIPARTITESTATS_H
+
+/*****************************************************************************
+ * 
+ * File:    attrbipartiteStats.h
+ * Author:  Alex Stivala
+ * Created: January 2025
+ *
+ *
+ * statistics functions (summing change statistics is verified against these)
+ *
+ ****************************************************************************/
+
+#include "utils.h"
+#include "graph.h"
+
+
+/* Approximate double floating point equality */
+#define DOUBLE_APPROX_EQ_TEST(a, b) ( fabs((a) - (b)) <= 1e-08 )
+
+double BipartiteExactlyOneNeighbourA(const graph_t *g, uint_t a);
+double BipartiteExactlyOneNeighbourB(const graph_t *g, uint_t a);
+double BipartiteTwoPathExactlyOneNeighbourA(const graph_t *g, uint_t a);
+
+#endif /* ATTRBIPARTITESTATS_H */
