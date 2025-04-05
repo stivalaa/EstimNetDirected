ea89e00b7cad218ccaf178d960d31a706e8b4527
Alex Stivala <alex.d.stivala@gmail.com>
2025-04-05 14:11:54 +1100

    use fill on factor A or B to have ggplot use same scale on one boxplot aes

--- examples/bipartite/simulation/plot_asymmetric_BipartiteFourCyclesNodePower_simulation_results_uniquenodes.R
+++ examples/bipartite/simulation/plot_asymmetric_BipartiteFourCyclesNodePower_simulation_results_uniquenodes.R
@@ -39,14 +39,30 @@ dat <- read.table('unique_bipartite_nodes_count_table_bipartite_asymmetric.txt',
 
 dat$theta_BipartiteFourCyclesNodePowerAxB<- interaction(dat$Aparam, dat$Bparam)
 
+## make data frame with two copies of the data, one for A nodes and one for
+## B nodes with unique A and B node counts respectively so that we can use
+## the ggplot2 colour or fill for factor A or B (using colour as some boxplots
+## have little variance so just black line on fill, using colour instead
+## they are red or blue).
+
+xdat <- dat
+xdat$nodeset <- 'A'
+ydat <- dat
+ydat$nodeset <- 'B'
+xdat$unique_count <- dat$uniqueFourCyclesA
+ydat$unique_count <- dat$uniqueFourCyclesB
+dat2 <- rbind(xdat,ydat)
+dat2$nodeset <- factor(dat2$nodeset)
 
 
-p <- ggplot(data = dat[dat$theta_BipartiteFourCyclesNodePowerAxB != "neg.neg",],
+
+## remove neg.neg case to match other plots, these have no 4-cycles anyway
+p <- ggplot(data = dat2[dat2$theta_BipartiteFourCyclesNodePowerAxB != "neg.neg",],
             aes(x = theta_BipartiteFourCyclesNodePowerAxB)) +
-  geom_boxplot(aes(y = uniqueFourCyclesA), color='red') +
-  geom_boxplot(aes(y = uniqueFourCyclesB), color='blue') +
+  geom_boxplot(aes(y = unique_count, color = nodeset)) +
   ylab('Unique A (red) or B (blue) nodes in four-cycles') +
-  xlab("BipartiteFourCyclesNodePower A and B parameter signs") 
+  xlab("BipartiteFourCyclesNodePower A and B parameter signs") +
+  scale_colour_manual(values = c('red','blue'), name = "Node set")
 
 postscript("bipartite_asymmetric_fourcyclesnodepower_simulation_uniquenodes_boxplots.eps", horizontal=FALSE, paper="special", width=9, height=6)
 print(p)
